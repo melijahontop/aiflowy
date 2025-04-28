b2c17fce6aa207086fd11c892223bb5f6a0af8b7
Michael Yang <fuhai999@gmail.com>
2025-04-28 15:53:21 +0800

    feat: DocNode 支持用户自定义输出名称

--- aiflowy-modules/aiflowy-module-ai/src/main/java/tech/aiflowy/ai/node/DocNode.java
+++ aiflowy-modules/aiflowy-module-ai/src/main/java/tech/aiflowy/ai/node/DocNode.java
@@ -1,13 +1,16 @@
 package tech.aiflowy.ai.node;
 
 import com.agentsflex.core.chain.Chain;
+import com.agentsflex.core.chain.Parameter;
 import com.agentsflex.core.chain.node.BaseNode;
+import com.agentsflex.core.util.StringUtil;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import tech.aiflowy.ai.utils.DocUtil;
 
 import java.io.ByteArrayInputStream;
 import java.util.HashMap;
+import java.util.List;
 import java.util.Map;
 
 public class DocNode extends BaseNode {
@@ -26,7 +29,14 @@ public class DocNode extends BaseNode {
         String url = map.get("fileUrl").toString();
         byte[] bytes = DocUtil.downloadFile(url);
         String docContent = readDocService.read(DocUtil.getFileNameByUrl(url), new ByteArrayInputStream(bytes));
-        res.put("content", docContent);
+
+        String key = "content";
+        List<Parameter> outputDefs = getOutputDefs();
+        if (outputDefs != null && !outputDefs.isEmpty()) {
+            String defName = outputDefs.get(0).getName();
+            if (StringUtil.hasText(defName)) key = defName;
+        }
+        res.put(key, docContent);
         return res;
     }
 }
