6414b01725e42cd4113c22c76f4d545c40b93736
Michael Yang <fuhai999@gmail.com>
2025-05-15 11:05:22 +0800

    docs: 文档站点，添加点击图片放到的功能

---
+++ docs/.vitepress/theme/Layout.vue
@@ -0,0 +1,24 @@
+<script setup lang="ts">
+import DefaultTheme from 'vitepress/theme'
+import {onMounted, nextTick, watch} from 'vue';
+import {useRoute} from 'vitepress'
+import mediumZoom from "medium-zoom";
+
+const route = useRoute()
+const initZoom = () => {
+  mediumZoom('.main img', {background: 'var(--vp-c-bg)'})
+};
+onMounted(() => {
+  initZoom()
+})
+watch(
+    () => route.path,
+    () => nextTick(() => {
+      initZoom()
+    })
+)
+</script>
+
+<template>
+  <DefaultTheme.Layout/>
+</template>
