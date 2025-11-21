a26508c79f01340bc5671f669cf438e3712b7f1a
john <409225776@qq.com>
2025-11-21 17:54:10 +0800

    chore: icon

--- aiflowy-ui-admin/packages/@core/base/shared/src/constants/aiflowy.ts
+++ aiflowy-ui-admin/packages/@core/base/shared/src/constants/aiflowy.ts
@@ -11,7 +11,8 @@ export const APP_DOC_URL = 'https://doc.aiflowy.tech';
 /**
  * @zh_CN AIFlowy Logo
  */
-export const APP_LOGO_URL = '/favicon.svg';
+export const APP_LOGO_URL =
+  'https://static.agentscenter.cn/attachment/1/2025/11/21/bebbe37b-f2d8-4776-9d51-d7a3535f4fe9/tabIcon.svg';
 
 /**
  * @zh_CN AIFlowy Admin 首页地址
